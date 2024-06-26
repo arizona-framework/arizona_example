85fe4899c8b72845c39b31a388a3738e6846b9ea
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-06-26 22:50:42 +0100

    Showcase Arizona via an example project

---
+++ README.md
@@ -0,0 +1,8 @@
+# `arizona_example`
+
+This is an example project (`rebar3`-based) for showcasing [Arizona](https://github.com/arizona-framework/arizona)'s
+capabilities, always aiming at the tip of its `main` branch (which is why there's no CHANGELOG).
+
+To run it, boot the Erlang/`rebar3` shell: e.g. `rebar3 shell`.
+
+If no errors were shown, the application should now be served at `localhost:8080`.
---
+++ src/arizona_example_app.erl
@@ -0,0 +1,19 @@
+%%%-------------------------------------------------------------------
+%% @doc arizona_example public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(arizona_example_app).
+
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_StartType, _StartArgs) ->
+    arizona_example_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions

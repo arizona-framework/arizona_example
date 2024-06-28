c4d15905ce69f5c0b3f08b159f986c670ede124e
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-06-28 22:32:38 +0100

    Fix `-spec(_).`s (#9)

--- src/arizona_example_app.erl
+++ src/arizona_example_app.erl
@@ -10,9 +10,16 @@
 -export([start/2]).
 -export([stop/1]).
 
+-spec start(StartType, StartArgs) -> Started
+    when StartType :: normal,
+         StartArgs :: [],
+         Started :: supervisor:startlink_ret().
 start(_StartType, _StartArgs) ->
     arizona_example_sup:start_link().
 
+-spec stop(State) -> Stopped
+    when State :: [],
+         Stopped :: ok.
 stop(_State) ->
     ok.
 
--- src/arizona_example_live_counter.erl
+++ src/arizona_example_live_counter.erl
@@ -17,15 +17,20 @@
 %% arizona_live_view callbacks.
 %% --------------------------------------------------------------------
 
-mount(#{assigns := Assigns} = Socket) ->
-    Count = maps:get(count, Assigns, 0),
-    {ok, arizona_socket:assign(count, Count, Socket)}.
+-spec mount(Socket) -> Mounted
+    when Socket :: arizona_socket:t(),
+         Mounted :: {ok, arizona_socket:t()}.
+mount(Socket) ->
+    Count = arizona_socket:get_assign(count, Socket, 0),
+    {ok, arizona_socket:put_assign(count, Count, Socket)}.
 
+-spec render(Macros) -> Tree
+    when Macros :: arizona_live_view:macros(),
+         Tree :: arizona_live_view:tree().
 render(Macros0) ->
-    Macros = Macros0#{
-        title => maps:get(title, Macros0, ~"Arizona")
-    },
-    ?ARIZONA_LIVEVIEW(~"""
+    Title = arizona_live_view:get_macro(title, Macros0, ~"Arizona"),
+    Macros = arizona_live_view:put_macro(title, Title, Macros0),
+    ?ARIZONA_LIVEVIEW(Macros, ~"""
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -50,31 +55,41 @@ render(Macros0) ->
     </html>
     """).
 
-handle_event(<<"incr">>, #{}, #{assigns := Assigns} = Socket) ->
-    Count = maps:get(count, Assigns) + 1,
-    {noreply, arizona_socket:assign(count, Count, Socket)};
-handle_event(<<"decr">>, #{}, #{assigns := Assigns} = Socket) ->
-    Count = maps:get(count, Assigns) - 1,
-    {noreply, arizona_socket:assign(count, Count, Socket)}.
+-spec handle_event(EventName, Payload, Socket) -> Handled
+    when EventName :: binary(),
+         Payload :: arizona:payload(),
+         Socket :: arizona_socket:t(),
+         Handled :: {noreply, arizona_socket:t()}.
+handle_event(<<"incr">>, _Payload, Socket) ->
+    Count = arizona_socket:get_assign(count, Socket) + 1,
+    {noreply, arizona_socket:put_assign(count, Count, Socket)};
+handle_event(<<"decr">>, _Payload, Socket) ->
+    Count = arizona_socket:get_assign(count, Socket) - 1,
+    {noreply, arizona_socket:put_assign(count, Count, Socket)}.
 
 %% --------------------------------------------------------------------
 %% Component functions.
 %% --------------------------------------------------------------------
 
+-spec counter(Macros) -> Tree
+    when Macros :: arizona_live_view:macros(),
+         Tree :: arizona_live_view:tree().
 counter(Macros) ->
-    ?ARIZONA_LIVEVIEW(~s"""
+    ?ARIZONA_LIVEVIEW(Macros, ~s"""
     <div :stateful>
         <div>Count: {_@count}</div>
         <.button event={_@event} text={_@btn_text} />
     </div>
     """).
 
+-spec button(Macros) -> Tree
+    when Macros :: arizona_live_view:macros(),
+         Tree :: arizona_live_view:tree().
 button(Macros) ->
-    ?ARIZONA_LIVEVIEW(~s"""
+    ?ARIZONA_LIVEVIEW(Macros, ~s"""
     {% NOTE: On this example, :onclick is and expression to be }
     {%       dynamic. It could be just, e.g., :onclick="incr". }
     <button type="button" :onclick={arizona_js:send(_@event)}>
         {_@text}
     </button>
     """).
-

6366687af65683f697c9e935896645268340fcca
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-06-30 19:13:16 +0100

    Improve existing specs

--- src/arizona_example_app.erl
+++ src/arizona_example_app.erl
@@ -5,14 +5,14 @@
 -export([stop/1]).
 
 -spec start(StartType, StartArgs) -> Started
-    when StartType :: normal,
-         StartArgs :: [],
+    when StartType :: application:start_type(),
+         StartArgs :: term(),
          Started :: supervisor:startlink_ret().
 start(_StartType, _StartArgs) ->
     arizona_example_sup:start_link().
 
 -spec stop(State) -> Stopped
-    when State :: [],
+    when State :: term(),
          Stopped :: ok.
 stop(_State) ->
     ok.

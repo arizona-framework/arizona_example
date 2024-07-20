129f092db41f2de750c47593c2f947c9ba0da5b9
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-07-20 01:28:31 +0100

    Allow testing our JS elements using a headless browser

---
+++ test/test.js
@@ -0,0 +1,50 @@
+/* global require */
+async function forCurCount(page, spanId) {
+    const count = await page.$eval(spanId, span => span.innerText);
+    console.log(`${spanId} has value ${count}.`)
+    return parseInt(count)
+}
+
+async function btnClick(page, btnId) {
+  console.log(`Clicking ${btnId}...`)
+  await page.click(btnId)
+}
+
+async function launchAt(location) {
+  const browser = await require('puppeteer').launch();
+  const page = await browser.newPage();
+  await page.goto(location);
+  return [browser, page]
+}
+
+async function teardown(browser) {
+  await browser.close();
+}
+
+async function forNewCount(page, spanId, value0, timeout) {
+  await page.waitForFunction(
+    (selector, value) => {
+      console.log(`${value}`)
+      return window.document.querySelector(selector).innerText === `${value}`
+    },
+    {timeout: timeout},
+    spanId,
+    value0
+  );
+}
+
+(async () => {
+  const location = 'http://localhost:8080'
+  const spanId = 'span#count_incr';
+  const btnId = 'button#btn_incr';
+  let count;
+
+  const [browser, page] = await launchAt(location);
+
+  count = await forCurCount(page, spanId)
+  await btnClick(page, btnId);
+  await forNewCount(page, spanId, count + 1, 1250)
+  await forCurCount(page, spanId)
+
+  await teardown(browser)
+})()

4d09f1897ad4aab9965442712f80860ef977ebb3
William Fank Thomé <williamthome@hotmail.com>
2025-02-23 17:23:21 -0300

    Format and lint files (#90)

--- priv/static/assets/js/main.js
+++ priv/static/assets/js/main.js
@@ -1,9 +1,9 @@
 /*global arizona*/
-"use strict";
+'use strict';
+
+arizona.subscribe('connect', () => {
+  console.info("[Client] I'm connected!");
+});
 
 const connectParams = {};
 arizona.connect(connectParams);
-
-arizona.subscribe("connect", () => {
-  console.info("[Client] I'm connected!");
-});

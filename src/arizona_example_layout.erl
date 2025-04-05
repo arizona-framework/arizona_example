11b283e1774e4b1cef23782cc3ddab721f651147
William Fank Thomé <willegp88@gmail.com>
2025-04-05 10:04:45 -0300

    Make it compatible with the bundler implementation (#101)

--- src/arizona_example_layout.erl
+++ src/arizona_example_layout.erl
@@ -26,7 +26,7 @@ render(View) ->
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{arizona:get_binding(title, View)}</title>
-        {arizona:render_html_scripts()}
+        <script src="assets/js/arizona/main.js"></script>
         <script src="assets/js/main.js"></script>
     </head>
     <body style="height: 100%; margin: 0;">

9a96abc6c946eb8c5af5eb36f2b7d1f84e666f44
William Fank Thomé <willegp88@gmail.com>
2025-10-05 15:46:03 -0300

    Update to latest Arizona breaking changes (#165)

--- priv/static/assets/js/main.js
+++ priv/static/assets/js/main.js
@@ -1,17 +1,10 @@
 /* global arizona */
 
 // Import the new Arizona client
-import Arizona from '/assets/js/arizona.min.js';
+import Arizona from '/assets/arizona/js/arizona.min.js';
 
 // Initialize the Arizona client
 globalThis.arizona = new Arizona();
 
 // Connect to the WebSocket server
 arizona.connect('/live');
-
-document.addEventListener('arizonaEvent', (event) => {
-  const { type, data } = event.detail;
-  if (type !== 'reply') return;
-  if (typeof data?.reload !== 'string') return;
-  window.location.reload();
-});
--- src/arizona_example_layout.erl
+++ src/arizona_example_layout.erl
@@ -5,36 +5,36 @@
 -ignore_xref([render/1]).
 
 -spec render(Bindings) -> Template when
-    Bindings :: arizona_binder:bindings(),
+    Bindings :: map(),
     Template :: arizona_template:template().
 render(Bindings) ->
-    arizona_template:from_html(~""""
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>{arizona_template:get_binding(title, Bindings)}</title>
-        <style>
-        html \{
-            height: 100%;
-        }
-        body \{
-            height: 100%;
-            margin: 0;
-        }
-        main \{
-           display: flex;
-           flex-direction: column;
-           row-gap: 1rem;
-           padding: 1rem;
-        }
-        </style>
-        <script type="module" src="/assets/example/js/main.js"></script>
-    </head>
-    <body>
-        {arizona_template:render_slot(arizona_template:get_binding(inner_content, Bindings))}
-    </body>
-    </html>
-    """").
+    arizona_template:from_erl([
+        ~"<!DOCTYPE html>",
+        {html, [{lang, ~"en"}], [
+            {head, [], [
+                {meta, [{charset, ~"UTF-8"}]},
+                {meta, [{'http-equiv', ~"X-UA-Compatible"}, {content, ~"IE=edge"}]},
+                {meta, [{name, ~"viewport"}, {content, ~"width=device-width, initial-scale=1.0"}]},
+                {title, [], arizona_template:get_binding(title, Bindings)},
+                {style, [], ~"""
+                html {
+                    height: 100%;
+                }
+                body {
+                    height: 100%;
+                    margin: 0;
+                }
+                main {
+                   display: flex;
+                   flex-direction: column;
+                   row-gap: 1rem;
+                   padding: 1rem;
+                }
+                """},
+                {script, [{type, ~"module"}, {src, ~"/assets/js/main.js"}], []}
+            ]},
+            {body, [], [
+                arizona_template:render_slot(arizona_template:get_binding(inner_content, Bindings))
+            ]}
+        ]}
+    ]).
